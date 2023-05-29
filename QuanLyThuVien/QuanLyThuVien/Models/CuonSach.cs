69d39cc9a5f84c5aab9cdf80c8a2da54dc7c6811
Trần Lê Duy <trn.duyle@gmail.com>
2023-05-29 14:56:12 +0700

    update model

--- QuanLyThuVien/QuanLyThuVien/Models/CuonSach.cs
+++ QuanLyThuVien/QuanLyThuVien/Models/CuonSach.cs
@@ -29,7 +29,8 @@ namespace QuanLyThuVien.Models
         public string ma_loaisach { get; set; }
         public string TinhTrang { get; set; }
         public string Mota { get; set; }
-    
+        public string Hinhmota { get; set; }
+
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<ChiTietMuonTra> ChiTietMuonTras { get; set; }
         public virtual DauSach DauSach { get; set; }
