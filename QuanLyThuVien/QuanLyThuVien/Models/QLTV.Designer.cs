ca8390400f6109abbc8bf6e63aef128da18ba753
Trần Lê Duy <trn.duyle@gmail.com>
2023-05-29 15:03:00 +0700

    .

--- QuanLyThuVien/QuanLyThuVien/Models/QLTV.Designer.cs
+++ QuanLyThuVien/QuanLyThuVien/Models/QLTV.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\D Drive\Projects\CNPM-QLTV\LIBRARY_MANAGEMENT\QuanLyThuVien\QuanLyThuVien\Models\QLTV.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\WEB - PROJECT\LIBRARY_MANAGEMENT\QuanLyThuVien\QuanLyThuVien\Models\QLTV.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
